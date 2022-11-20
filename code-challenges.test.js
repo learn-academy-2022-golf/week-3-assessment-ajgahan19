12a0d1f1492bdc4b8f390980a7332c67ad253e54
ajgahan19 <andregahan77@gmail.com>
2022-11-20 12:35:35 -0800

    DONE

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,44 +14,108 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fibLength1 = 6
-// Expected output: [1, 1, 2, 3, 5, 8]
+describe("greaterNumber", () => {
+  it ("takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.", () => {
+    const fibLength1 = 6
+    // Expected output: [1, 1, 2, 3, 5, 8]
+    const fibLength2 = 10
+    // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    expect(greaterNumber(6)).toEqual([1, 1, 2, 3, 5, 8])
+    expect(greaterNumber(10)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+  }
+  )
+}
+)
+// ReferenceError: greaterNumber is not defined
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 total
 
-const fibLength2 = 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+// Pseudocode:
+// Create a function called greaterNumber
+// The function has one parameter 'n' that indicates the number that will be passed into the function
+// Create a variable fib to identify the numbers we already know in the sequence, 1 and 1.
+// Create a conditional statement to determine if:
+  // Identify of the input is set at the value of greater than 2
+    // if the number is greater than 2 create a for loop to iterate through the array of the given number until we have the index of the given number.
+      // We know that the index will start at the index of 2 since the first two numbers are already given to us and their indexes are 0 and 1 respectively. 
+    // For each iteration, we want to populate the index of i so fib at the index of i is set to the previous two numbers added together.
+      // Return this value of fib
+  // If the number is 2 or lower
+    // return "No way, Jose"
 
-// b) Create the function that makes the test pass.
+  const greaterNum = (n) => {
+    let fib = [1,1]
+    if (n>2)
+    for (let i=2; i<n; i++) {
+      fib[i] = fib[i-1] + fib[i-2]}
+    return fib
+    if (n<2) {
+      return "No way, Jose"
+    }
+  }
+  // Test Suites: 1 passed, 1 total
+  // Tests:       1 passed, 1 total
+  // When I log a number less than 2 I still get an array of the same length as the number I input. If I didn't add a conditional statement to the function, then I would feel more confident. However, I tried adding curly brackets, but I still got an unexpected error and I can't move line because the variable has to be defined inside the function. I'm not sure how to fix this.
 
+
+
+
+//DONE Friday 11:00pm -Reviewed saturday @ 9:00am
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const studyMinutesWeek1 = {
-  sunday: 90,
-  monday: 30,
-  tuesday: 20,
-  wednesday: 15,
-  thursday: 30,
-  friday: 15,
-  saturday: 60
-}
-// Expected output: [15, 15, 20, 30, 30, 60, 90]
-
-const studyMinutesWeek2 = {
-  sunday: 100,
-  monday: 10,
-  tuesday: 45,
-  wednesday: 60,
-  thursday: 20,
-  friday: 15,
-  saturday: 65
-}
+// a) Create a test with expect statements for each of the variables provided.
+describe("studyTime", () => {
+  it("takes in an object and returns an array of the values sorted from least to greatest.", () => {
+  const studyMinutesWeek1 = {
+    sunday: 90,
+    monday: 30,
+    tuesday: 20,
+    wednesday: 15,
+    thursday: 30,
+    friday: 15,
+    saturday: 60
+  }
+  // Expected output: [15, 15, 20, 30, 30, 60, 90]
+  const studyMinutesWeek2 = {
+    sunday: 100,
+    monday: 10,
+    tuesday: 45,
+    wednesday: 60,
+    thursday: 20,
+    friday: 15,
+    saturday: 65
+  }
 // Expected output: [10, 15, 20, 45, 60, 65, 100]
+  expect(studyTime(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90])
+  expect(studyTime(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100])
+})
+}
+)
+// ReferenceError: studyTime is not defined
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 passed, 2 total
 
-// b) Create the function that makes the test pass.
+// Psuedo: carrying my inital thoughts down, I think that I need to create a function that takes in an object as an argument. 
+//I need to return an array of the values sorted from least to greatest. I need to use the object.values() method to get the values from the object. 
+//I need to use the sort method to sort the values from least to greatest. I need to return the sorted array.
+// Create a function called studyTime
+// Has one parameter:an object
+// Declare a variable orderedTime that is set at the Object.values to initially list out all the elements in the input array.
+  // Use the .sort() method to sort the array from smallest to largest 
+      // The .sort() method will automatically sort the numbers in the array from smallest to largest, with one caveat. .sort recognizes 100 as larger than 20 becuase it compares first character to first character (1 > 2). Thus, to fix this, we have to use the compare function. The compare function compares two VALUES at a time (a, b). If we were to compare 50 and 100, the sort method would subtract a from b 50 -100  and would return a negative number, which sort() would then recognize 50 as the lower number.
 
-// --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
+const studyTime = (object) => {
+let orderedTime = Object.values(object).sort(function(a, b) {return a - b})
+return orderedTime}
+
+// }Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
+
+//DONE Friday 12:20pm -Reviewed saturday @ 10:30am refactored fixed SUnday 11:59am / completely rewritten  fixed SUnday 2:49pm
+// --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty 
 
 // a) Create a test with expect statements for each of the variables provided.
 
@@ -64,4 +128,61 @@ const accountTransactions2 = [250, -89, 100, -96]
 const accountTransactions3 = []
 // Expected output: []
 
+// Input1: [100, -17, -23, -9]
+// Output1: [100, 83, 60, 51]
+
+// Input2: [250, -89, 100, -96]
+// Output2: [250, 161, 261, 165]
+
+// Input3: []
+// Output3: []
+
+describe("accTran1", () => {
+  it("takes in an array and returns an array of the accumulating sum", () => {
+    let input1 = [100, -17, -23, -9]
+    let expectedOutput1 = [100, 83, 60, 51]
+    expect(accTran1(input1)).toEqual(expectedOutput1)
+  })
+})
+
+describe("accTran2", () => {
+  it("takes in an array and returns an array of the accumulating sum", () => {
+    let input2 = [250, -89, 100, -96]
+    let expectedOutput2 = [250, 161, 261, 165]
+    expect(accTran2(input2)).toEqual(expectedOutput2)
+  })
+})
+
+describe("accTran3", () => {
+  it("takes in an array and returns an array of the accumulating sum", () => {
+    let input3 = []
+    let expectedOutput3 = []
+    expect(accTran3(input3)).toEqual(expectedOutput3)
+  })
+})
+
+// ReferenceError: accTran(1,2,3) is not defined
+
 // b) Create the function that makes the test pass.
+
+// Pseudocode:
+// Create functions called accTran1,2,&3 that takes in an array and returns an array of the accumulating sum
+// Use the .map method to iterate over the array
+// Use an arrow function to evaluate the sum accumulating sum of each array
+// Return the new array or the empty array
+
+const accTran1 = (array1) => {
+  return array1.map((sum => value => sum += value)(0))
+}
+
+const accTran2 = (array2) => {
+  return array2.map((sum => value => sum += value)(0))
+}
+
+const accTran3 = (array3) => {
+  return array3.map((sum => value => sum += value)(0))
+
+
+// For the above problems I pseudocoded to the best of my ability. 
+// This past week during white boards austin said the way we all went and pseudocoded out problem though white boarding wasn't the best way to go about /doing it. 
+// So was woundering if you could give us some tips during class this next week. Just like when we got that refresher on how a driver and navagator should handle things.
--- interview-questions.md
+++ interview-questions.md
@@ -2,6 +2,144 @@
 
 Answer the following questions.
 
+First, without external resources. Challenge yourself to answer from memory as if you were in a job interview. 
+Then, research the question to expand on your answer. Even if you feel you have answered the question completely, there is always something more to learn. Write your researched answer in your OWN WORDS.
+
+
+1. What is JSX? What is one syntax difference between HTML and JSX?
+
+Your answer: 
+JSX is a tool that allows Devlopers to use javascript and html elements in the same code. To use javascript functionality in JSX, you would need to wrap it in curly braces.
+
+Researched answer: JSX is a syntax extension of Javascript designed to build components in React. It allows a combination of HTML tags and javascript functionalit What is yarn? What file(s) are modified in a React application when you run the command yarn in your terminal?
+
+Your answer: yarn is a tool used from the terminal to install and run React apps. It can install the various modules, .lock and .json file associated with React.
+
+Researched answer: Yarn is a package manager for React that fetches and downloads dependencies for creating a React app.
+3. What is an anonymous function in JavaScript?
+
+Your answer: An anonymous function is an unnamed function, as in it is not directly attached to a variable. I believe, for example, the functions inside higher order functions would be considered anonymous.
+
+Researched answer: Anonymous functions are not named, and cannot be accessed again after their creation unless invoked through a parent function.
+
+4. What is the difference between state values and props in React?
+Your answer: State is data saved locally through user input, props are bits of data made available across React components through props syntax.
+
+Researched answer: State is data saved locally to its componet only. Props passes data between components.
+
+5. What is the DOM?
+Your answer: DOM stands for document object model.  
+
+Researched answer: The DOM is a representation of a created web page that can be displayed in the browser during development.
+
+6. STRETCH: Which is the difference between a div and a span?
+Your answer: A div is used to "lump" together elements in html and jsx as a single customizable element. I'm not sure when it comes to span, but will research it and can get a better answer.
+Researched answer: They are similar in their use for grouping elements together for common styling and use, but span is an inline element.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ASSESSMENT 3: Interview Practice Questions
+
+Answer the following questions.
+
 First, without external resources. Challenge yourself to answer from memory as if you were in a job interview.
 
 Then, research the question to expand on your answer. Even if you feel you have answered the question completely, there is always something more to learn. Write your researched answer in your OWN WORDS.
@@ -20,7 +158,7 @@ Researched answer:
 
 3. What is an anonymous function in JavaScript?
 
-Your answer:
+Your answer: An anonymous function in Javascript is     
 
 Researched answer:
 
@@ -32,7 +170,7 @@ Researched answer:
 
 5. What is the DOM?
 
-Your answer:
+Your answer: 
 
 Researched answer:
 
@@ -46,10 +184,10 @@ Researched answer:
 
 1. Object-oriented programming:
 
-2. Ruby:
+2. Ruby: 
 
 3. Implicit return:
 
-4. Ruby blocks:
+4. Ruby blocks: Ruby blocks 
 
 5. Ruby hashes:
